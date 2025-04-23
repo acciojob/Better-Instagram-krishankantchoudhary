0fa6cca82f09ef4766cc3b7c557c294a0aadf662
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2025-04-23 15:51:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your code here
+let draggedElement=null;
+document.querySelectorAll('.image').forEach(image=>{
+	image.addEventListener("dragstart",e=>{
+		draggedElement=e.target;
+		draggedElement.classList.add('selected');
+	});
+
+	image.addEventListener("dragover",e=>{
+		e.preventDefault();
+	});
+
+	image.addEventListener("drop",e=>{
+		e.preventDefault();
+
+		if(draggedElement && draggedElement !==e.target){
+			let draggedBg=draggedElement.style.backgroundImage;
+			draggedElement.style.backgroundImage=e.target.style.backgroundImage;
+			e.target.style.backgroundImage=draggedBg;
+		}
+		draggedElement.classList.remove("selected");
+	});
+
+	image.addEventListener('dragend',()=>{
+		if(draggedElement){
+			draggedElement.classList.remove("selected");
+		}
+	});
+	
+	
+	
+});
+
